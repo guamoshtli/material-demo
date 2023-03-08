5038fe98294f415aaf3ce56ac2397f6599af94bc
gonzalo <gonzalo@mail.com>
2023-03-07 21:20:41 -0600

    MatCardModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { MatCardModule  } from '@angular/material/card'
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
